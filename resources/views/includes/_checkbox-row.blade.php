f52b15fcdd5ddacc43a6ce7c694103f95c5e0784
Mohamed Fawzan <fawzanm@gmail.com>
2020-07-10 07:04:57 +0400

    fix checkbox style

--- resources/views/includes/_checkbox-row.blade.php
+++ resources/views/includes/_checkbox-row.blade.php
@@ -1,3 +1,3 @@
-<td class="align-middle" style="width: 1%;">
+<td class="px-2" style="width: 1%;">
     <input type="checkbox" wire:model="checkboxValues" value="{{ $model->{$checkboxAttribute} }}">
 </td>
