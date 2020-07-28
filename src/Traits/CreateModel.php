be9ad99a8d026e99bb60ac29d8e630aadb59df55
jjrohrer <jj.bitbucket@rohrer.org>
2020-07-28 08:45:58 -0400

    updated obsersvations about the state of CreateModel

--- src/Traits/CreateModel.php
+++ src/Traits/CreateModel.php
@@ -3,11 +3,13 @@
 
 namespace ElegantTechnologies\LaravelLivewireTables\Traits;
 
-
+// 7/2020 - This was copied from https://github.com/fawzanm/laravel-livewire-tables, but it looks like unfinished work
+//  Don't use until your examine and refactor.
+//  This isn't really a reusable trait, but it does create a button when $createEnabled = true 
 trait CreateModel
 {
 
-    public $createEnabled = true;
-    public $createLabel = 'New Model';
+    public $createEnabled = false;
+    public string $createLabel = 'New Model';
     public $createRoute = '#';
 }
